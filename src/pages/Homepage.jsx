3c74631050000a700bca1de190ad5448c16fe9c5
Morad17 <morad123@hotmail.co.uk>
2022-03-18 16:32:03 +0000

    setting up redux and news

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,7 @@ import { Cryptocurrencies, News } from './'
 const { Title } = Typography
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats
 
   if(isFetching) return 'Loading...'
@@ -30,7 +30,7 @@ const Homepage = () => {
         <Title level={2} className="home-title">Top 10 CryptoCurrencies in the World</Title>
         <Title level={2} className="home-title"><Link to="/cryptocurrencies">Show Mores</Link></Title>
       </div>
-      <Cryptocurrencies simplified/>
+      <Cryptocurrencies simplified={true}/>
       <div className="home-heading-container">
         <Title level={2} className="home-title">Latest Crypto News</Title>
         <Title level={2} className="home-title"><Link to="/news">Show Mores</Link></Title>
